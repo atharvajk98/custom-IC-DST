e838482789e26c2627230280577fc80b6c4c9ee5
“mr-atharva-kulkarni” <atharva.j.kulkarni1998@gmail.com>
2023-09-19 12:21:11 -0400

    adding random exp v2, v3

--- scripts/run_gpt_turbo_zeroshot.sh
+++ scripts/run_gpt_turbo_zeroshot.sh
@@ -4,9 +4,9 @@
 SEED=0
 MWZ_VER=2.4
 NUM_EXAMPLES=10
-VERSION=2
+VERSION=1
 
-CUDA_VISIBLE_DEVICES=2 nohup python3 -u src/run_zeroshot_gpt_turbo_experiment.py \
+CUDA_VISIBLE_DEVICES=1 nohup python3 -u src/run_zeroshot_gpt_turbo_experiment.py \
 --seed $SEED \
 --random_exp \
 --mwz_ver $MWZ_VER \
